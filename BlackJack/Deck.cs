a887f5cbb756e3651b9e7f4dbcb7e6b2d5d6e53b
dansil03 <2100940dannullis@zuyd.nl>
2021-12-05 18:46:22 +0100

    Projektdateien hinzufügen.

---
+++ BlackJack/Deck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Deck
+    {
+        public List<Card> deck = new List<Card>();
+        public List<Card> Shuffled = new List<Card>();
+        
+        public Deck()
+        {
+
+            string[] suits = { "Harten", "Schoppen", "Klaver", "Ruiten" };
+            string[] ranksTien = {"Boer", "Koningin", "Koning" };
+            
+            foreach(string suit in suits)
+            {
+                foreach (string rank in ranksTien)
+                {                     
+                    Card card1 = new Card(suit, rank, 10);
+                    deck.Add(card1); 
+                }
+            }
+
+            foreach (var suit in suits)
+            {
+                for (int i = 2; i < 11; i++)
+                {                    
+                    Card card2 = new Card(suit, i.ToString(), i);
+                    deck.Add(card2); 
+                }
+            }
+            Shuffle(); 
+            
+        }
+        public void Shuffle()
+        {
+            for (int i = 0; i < 48; i++)
+            {
+                var random = new Random();
+                int r = random.Next(deck.Count);
+                Card RandomKaart = deck[r];
+                Shuffled.Add(RandomKaart);
+                deck.RemoveAt(r); 
+            }
+        }         
+	}         
+}
---
+++ BlackJack/Player.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Player
+    {
+        Deck deck1 = new Deck();
+        List<Card> HandKaarten = new List<Card>();
+        List<Card> DealerKaarten = new List<Card>();
+        public int PuntenTotaal;
+        public int PuntenDealer;
+
+
+
+
+
+
+
+
+       public Player()
+       {
+
+            Spel(); 
+       }
+
+        public void Spel()
+        {
+            void BeginSpel()
+            {
+                void BeginKaarten()
+                {
+                    HandKaarten.Add(deck1.Shuffled[0]);
+                    deck1.Shuffled.RemoveAt(0);
+                    HandKaarten.Add(deck1.Shuffled[0]);
+                    deck1.Shuffled.RemoveAt(0);
+
+                    int PuntenSpeler()
+                    {
+                        int Punt1 = HandKaarten[0].Value;
+                        int Punt2 = HandKaarten[1].Value;
+                        PuntenTotaal = Punt1 + Punt2;
+                        return PuntenTotaal;
+                    }
+                    PuntenSpeler(); 
+                    foreach(Card kaart in HandKaarten)
+                    {
+                        Console.WriteLine("player1: " + kaart.HeleKaart);
+                        
+                    }
+                    Console.WriteLine("Punten: " + PuntenSpeler());
+                }
+                void BeginDealer()
+                {
+                    DealerKaarten.Add(deck1.Shuffled[0]);
+                    deck1.Shuffled.RemoveAt(0);
+                    PuntenDealer = DealerKaarten[0].Value; 
+
+                    foreach(Card kaart in DealerKaarten)
+                    {
+                        Console.WriteLine("Dealer: " + kaart.HeleKaart);
+                        Console.WriteLine("Punten: " + PuntenDealer); 
+                    }                   
+                }
+                BeginKaarten();
+                BeginDealer();
+            }
+            BeginSpel();
+
+            void NieuweKaart()
+            {
+                if (PuntenTotaal == 21 )
+                {
+                    Dealer();  
+                }
+
+                else if (PuntenTotaal > 21)
+                {
+                    Console.WriteLine("je hebt te veel");
+                    
+                }
+
+                else
+                {
+                    Console.WriteLine("wilt u nog een kaart? ");
+                    string Antwoord = Console.ReadLine();
+
+                    if(Antwoord == "ja")
+                    {
+                        HandKaarten.Add(deck1.Shuffled[0]);
+                        deck1.Shuffled.RemoveAt(0); 
+                        
+                        foreach(var kaart in HandKaarten)
+                        {
+                            Console.WriteLine("speler1: " + kaart.HeleKaart); 
+                        }
+
+                        for (int i = 2; i < HandKaarten.Count; i++)
+                        {
+                            int NieuwPunten = HandKaarten[i].Value;
+                            PuntenTotaal = PuntenTotaal + NieuwPunten; 
+                        }
+
+                        Console.WriteLine("punten: " + PuntenTotaal);
+                        NieuweKaart();
+                    }
+
+                    else if(Antwoord == "nee")
+                    {
+                        Dealer(); 
+                    }
+                }
+            }
+            NieuweKaart(); 
+
+            void Dealer()
+            {
+                if (PuntenDealer < PuntenTotaal && PuntenDealer < 21)
+                {
+                    DealerKaarten.Add(deck1.Shuffled[0]);
+                    PuntenDealer = PuntenDealer + deck1.Shuffled[0].Value;
+                    deck1.Shuffled.RemoveAt(0); 
+                    foreach(var kaart in DealerKaarten)
+                    {
+                        Console.WriteLine("Dealer: " + kaart.HeleKaart); 
+                    }
+                    Console.WriteLine("punten: " + PuntenDealer);
+
+                    Dealer(); 
+                }
+                
+
+                if (PuntenDealer > 21)
+                {
+                    Console.WriteLine("je hebt gewonnen!"); 
+                }
+
+                else if(PuntenDealer <= 21 && PuntenDealer > PuntenTotaal)
+                {
+                    Console.WriteLine("je hebt verloren!"); 
+                }
+
+                else if(PuntenTotaal == PuntenDealer)
+                {
+                    Console.WriteLine("Push!"); 
+                }                
+            }            
+        }   
+    }
+}
